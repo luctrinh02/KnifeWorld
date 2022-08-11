261ebcebe3dcb9c1fbf48e390cc6673dd8f86918
DESKTOP-TO7ENMK\NTS <luctt@seatechit.com.vn>
2022-08-11 13:49:43 +0700

    bill history detail

--- src/main/java/com/luctt/KnifeWorld/api/AdminBillApi.java
+++ src/main/java/com/luctt/KnifeWorld/api/AdminBillApi.java
@@ -1,12 +1,65 @@
 package com.luctt.KnifeWorld.api;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.luctt.KnifeWorld.entities.Bill;
+import com.luctt.KnifeWorld.entities.BillDetail;
+import com.luctt.KnifeWorld.service.BillDetailService;
+import com.luctt.KnifeWorld.service.BillService;
+import com.luctt.KnifeWorld.utilities.GetMap;
+
+@Controller
 @RequestMapping("/api/admin/bills")
 public class AdminBillApi {
-	// @GetMapping("")
-	
+	@Autowired
+	private BillService billService;
+	@Autowired
+	private BillDetailService billDetailService;
+	@GetMapping("")
+	public String getBill(Model model,@RequestParam("page") Integer num) {
+		 Page<Bill> page=billService.getAll(num);
+		 model.addAttribute("list", page);
+		 return "admin/bill/indexView";
+	 }
+	@PatchMapping("/{id}")
+	public ResponseEntity<?> changeStatus(@PathVariable("id") Integer id,@RequestParam("status") Integer status){
+		Bill bill=billService.getById(id);
+		List<BillDetail> details=billDetailService.getByBil(bill);
+		HashMap<String, Object> map;
+		for(BillDetail x:details) {
+			if(x.getAmount()>x.getProduct().getAmount()) {
+				map=GetMap.getData("error", "Sản phẩm không đủ số lượng!");
+				return ResponseEntity.ok(map);
+			}
+		}
+		bill.setStatus(status);
+		billService.save(bill);
+		if(status==1) {
+			map=GetMap.getData("ok", "Đơn đã được chấp nhận");
+		}else {
+			map=GetMap.getData("ok", "Đơn đã được hủy");
+		}
+		return ResponseEntity.ok(map);
+	}
+	@GetMapping("/{id}")
+	public String getDetail(Model model,@PathVariable("id") Integer id) {
+		Bill bill=billService.getById(id);
+		List<BillDetail> billDetails=billDetailService.getByBil(bill);
+		model.addAttribute("obj", bill);
+		model.addAttribute("billDetails", billDetails);
+		return "user/detail";
+	}
 }
--- src/main/java/com/luctt/KnifeWorld/controller/BillController.java
+++ src/main/java/com/luctt/KnifeWorld/controller/BillController.java
@@ -2,6 +2,8 @@ package com.luctt.KnifeWorld.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -12,18 +14,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luctt.KnifeWorld.entities.Bill;
 import com.luctt.KnifeWorld.entities.BillDetail;
+import com.luctt.KnifeWorld.entities.User;
 import com.luctt.KnifeWorld.service.BillDetailService;
 import com.luctt.KnifeWorld.service.BillService;
+import com.luctt.KnifeWorld.service.UserService;
 
 @Controller
 public class BillController {
 	@Autowired
 	private BillService service;
 	@Autowired
+	private UserService userService;
+	@Autowired
 	private BillDetailService billDetailService;
 	@GetMapping("/bills")
-	public String getBill(Model model) {
-		Page<Bill> page=service.getAll(0);
+	public String getBill(Model model,@RequestParam(name="page",defaultValue = "0") Integer num,HttpServletRequest request) {
+		String email =request.getUserPrincipal().getName();
+		User u=(User) userService.getByEmail(email);
+		Page<Bill> page=service.getHistory(u,num);
 		model.addAttribute("list", page);
 		return "user/history";
 	}
@@ -46,5 +54,13 @@ public class BillController {
 		Page<Bill> bills=service.getAll(0);
 		model.addAttribute("list", bills);
 		return "admin/bill/index";
+	}
+	@GetMapping("/api/admin/users/{id}/bills")
+	public String getBillByUser(Model model,@PathVariable("id") Integer id,@RequestParam("page") Integer num) {
+		User u= userService.getById(id);
+		Page<Bill> page=service.getHistory(u,num);
+		model.addAttribute("list", page);
+		model.addAttribute("userId", u.getId());
+		return "admin/user/history";
 	}
 }
